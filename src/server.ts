7a26c80db0bd562e7545f0cbaf7fd092f0304233
Marcelo Fabiano <marcelofabianov@gmail.com>
2023-05-06 11:42:23 -0300

    build: update package.json scripts and add nodemon ts-node-dev swc build

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    helloWorld: String!
+  }
+`
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query: {
+      helloWorld: () => "Hello, world!"
+    }
+  }
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
